7c92e9b685fda285912925e918b29c25d4b4f9c5
dioo1461 <dioo1461@gmail.com>
2024-11-07 18:12:18 +0900

    docs(SelectableBottomSheet.tsx): 주석 추가

--- one_bucket/src/components/bottomSheet/SelectableBottomSheet.tsx
+++ one_bucket/src/components/bottomSheet/SelectableBottomSheet.tsx
@@ -6,7 +6,6 @@ import {
     View,
     Platform,
 } from 'react-native'
-
 import BottomSheet from '@/components/bottomSheet/BottomSheet'
 import Line from '@/components/Line'
 
@@ -21,6 +20,51 @@ type SelectableBottomSheetProps = {
     }[]
 }
 
+/**
+ * SelectableBottomSheet
+ *
+ * 선택 가능한 버튼 목록을 하단 시트(Bottom Sheet) 형태로 보여주는 컴포넌트입니다.
+ * 각 버튼은 사용자 정의 동작을 트리거할 수 있으며, 이 컴포넌트는 주로 화면 하단에서 여러 옵션을 제공할 때 사용됩니다.
+ *
+ * @param enabled - Bottom Sheet의 표시 여부를 제어하는 boolean 값입니다.
+ * @param onClose - Bottom Sheet가 닫힐 때 호출되는 함수입니다.
+ * @param theme - 현재 테마에 따른 색상 정보를 담고 있는 객체로, Bottom Sheet의 스타일을 지정하는 데 사용됩니다.
+ * @param buttons - 버튼 목록을 설정하는 배열입니다. 각 버튼은 `text`, `style` ('default' 또는 'destructive'), `onPress` 함수 속성을 가집니다.
+ *
+ * @example
+ * ```typescript
+ * import { SelectableBottomSheet } from '@/components/bottomSheet/SelectableBottomSheet';
+ *
+ * const MyComponent = () => {
+ *     const [isBottomSheetVisible, setBottomSheetVisible] = useState(false);
+ *
+ *     const buttons = [
+ *         { text: '수정하기', style: 'default', onPress: handleEdit },
+ *         { text: '삭제하기', style: 'destructive', onPress: handleDelete }
+ *     ];
+ *
+ *     const handleEdit = () => {
+ *         // 수정 동작을 처리하는 함수
+ *     };
+ *
+ *     const handleDelete = () => {
+ *         // 삭제 동작을 처리하는 함수
+ *     };
+ *
+ *     return (
+ *         <View>
+ *             <Button title="Bottom Sheet 열기" onPress={() => setBottomSheetVisible(true)} />
+ *             <SelectableBottomSheet
+ *                 enabled={isBottomSheetVisible}
+ *                 onClose={() => setBottomSheetVisible(false)}
+ *                 theme={theme}
+ *                 buttons={buttons}
+ *             />
+ *         </View>
+ *     );
+ * };
+ * ```
+ */
 export const SelectableBottomSheet: React.FC<SelectableBottomSheetProps> = ({
     enabled,
     onClose,
