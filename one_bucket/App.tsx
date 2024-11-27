f6f818a46fd5c6e4f71496ae34587b34a60338ce
dioo1461 <dioo1461@gmail.com>
2024-11-27 22:25:20 +0900

    feat(App.tsx): Navigation Deep Linking 적용

--- one_bucket/App.tsx
+++ one_bucket/App.tsx
@@ -258,15 +258,57 @@ function App(): React.JSX.Element {
         )
     }
 
+    const linking = {
+        prefixes: ['app://'], // 앱에서 처리할 URL의 prefix
+        config: {
+            screens: {
+                // 네스트된 네비게이터를 정의할 경우 올바른 타입으로 설정
+
+                Home: '', // 기본 루트 경로
+                Search: 'search/:keyword/:option', // 검색 페이지
+                Notification: 'notification', // 알림 페이지
+                ProfileDetails: 'profile/:userId', // 사용자 프로필 상세
+
+                // 개별 스크린 매핑
+                [strings.unauthHomeScreenName]: 'unauth/home',
+                [strings.createGroupTradePostScreenName]:
+                    'group-trade/create-post',
+                [strings.groupTradePostScreenName]: 'group-trade/:postId',
+                [strings.profileDetailsScreenName]: 'profile/:userId',
+                [strings.profileModifyScreenName]: 'profile/modify',
+                [strings.settingScreenName]: 'settings',
+                [strings.schoolAuth1ScreenName]: 'auth/school/step1',
+                [strings.schoolAuth2ScreenName]: 'auth/school/step2',
+                [strings.schoolAuth3ScreenName]: 'auth/school/step3',
+                [strings.changePwScreenName]: 'auth/change-password/step1',
+                [strings.changePw2ScreenName]: 'auth/change-password/step2',
+                [strings.alertSettingScreenName]: 'settings/alerts',
+                [strings.announcementListScreenName]: 'announcements',
+                [strings.announcementPostScreenName]: 'announcement/:postId',
+                [strings.supportScreenName]: 'support',
+                [strings.versionCheckScreenName]: 'settings/version',
+                [strings.boardCreatePostScreenName]: 'board/create-post',
+                [strings.updateBoardPostScreenName]:
+                    'board/update-post/:postId',
+                [strings.boardPostScreenName]: 'board/:postId',
+                [strings.imageEnlargementScreenName]: 'image-enlargement',
+                [strings.chatScreenName]: 'chat/:roomId',
+                [strings.myLikedPostsScreenName]: 'profile/liked-posts',
+                [strings.myBoardPostsScreenName]: 'profile/board-posts',
+                [strings.myGroupTradePostsScreenName]:
+                    'profile/group-trade-posts',
+            },
+        },
+    }
+
     return (
         <QueryClientProvider client={queryClient}>
-            {loginState ? (
-                <NavigationContainer
-                    theme={
-                        themeColor === lightColors
-                            ? lightNavTheme
-                            : darkNavTheme
-                    }>
+            <NavigationContainer
+                linking={linking}
+                theme={
+                    themeColor === lightColors ? lightNavTheme : darkNavTheme
+                }>
+                {loginState ? (
                     <Stack.Navigator>
                         <Stack.Screen
                             name='Main'
@@ -581,14 +623,7 @@ function App(): React.JSX.Element {
                             component={MyGroupTradePosts}
                         />
                     </Stack.Navigator>
-                </NavigationContainer>
-            ) : (
-                <NavigationContainer
-                    theme={
-                        themeColor === lightColors
-                            ? lightNavTheme
-                            : darkNavTheme
-                    }>
+                ) : (
                     <Stack.Navigator
                         screenOptions={{ headerShown: false }}
                         initialRouteName={strings.loginScreenName}>
@@ -617,8 +652,8 @@ function App(): React.JSX.Element {
                             component={NewPw2}
                         />
                     </Stack.Navigator>
-                </NavigationContainer>
-            )}
+                )}
+            </NavigationContainer>
             <Toast position='bottom' bottomOffset={40} visibilityTime={1000} />
         </QueryClientProvider>
     )
